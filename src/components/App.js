e07427d9daf2602c45c9c1490d55c2bc0a3de73f
felixgren <33127919+felixgren@users.noreply.github.com>
2021-03-08 23:59:37 +0100

    Updated page info & added fonts

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import DefaultPage from './DefaultPage';
+
+const PageWrapper = styled.div``;
+
+function App() {
+    return (
+        <PageWrapper>
+            <DefaultPage />
+        </PageWrapper>
+    );
+}
+
+export default App;
---
+++ src/components/DefaultPage.js
@@ -0,0 +1,68 @@
+import logo from '../logo512.png';
+import styled from 'styled-components';
+
+const AppLol = styled.div`
+    text-align: center;
+    font-family: 'NewRocker';
+`;
+
+const AppHeader = styled.header`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(50px + 2vmin);
+    text-transform: uppercase;
+    color: black;
+`;
+
+const AppLogo = styled.img`
+    height: 40vmin;
+    pointer-events: none;
+
+    @media (prefers-reduced-motion: no-preference) {
+        animation: App-logo-spin infinite 40s linear;
+    }
+
+    @keyframes App-logo-spin {
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg);
+        }
+    }
+`;
+
+const AppLink = styled.a`
+    color: #61dafb;
+    font-family: 'Montserrat';
+    font-weight: 400;
+    transition: 0.25s;
+
+    &:hover {
+        font-weight: 500;
+    }
+`;
+
+const Default = () => {
+    return (
+        <AppLol>
+            <AppHeader>
+                <AppLogo src={logo} alt="logo" />
+                <p>mario time mario time mario time</p>
+                <AppLink
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn mario time
+                </AppLink>
+            </AppHeader>
+        </AppLol>
+    );
+};
+
+export default Default;
